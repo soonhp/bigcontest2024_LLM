9bee9874b732a37765239514851de3e40881e9c4
PassionA2Z <passiona2z@naver.com>
2024-10-20 21:56:17 +0900

    output_formatting_v2

--- cypher_query/retrieval_query.py
+++ cypher_query/retrieval_query.py
@@ -1,3 +1,4 @@
+# 기존버전
 retrievalQuery = """
 MATCH (node)<-[:HAS_REVIEW]-(store)
 RETURN node.text AS text,
@@ -11,5 +12,20 @@ RETURN node.text AS text,
          storeImage: store.image_url,
          storeRating: store.rating,
          score: score
+       } AS metadata
+"""
+
+# 수정버전(선진) 1020
+retrievalQuery_v2 = """
+MATCH (node)<-[:HAS_REVIEW]-(store)
+RETURN node.text AS text,
+       store AS store,
+       score,
+       {
+         reviewText: node.text,
+         storeName: store.MCT_NM,
+         store_Type: store.MCT_TYPE,	
+         store_Image: {kakao: store.image_url_kakao, google: store.image_url_google},
+         store_Rating: {kakao: store.rating_kakao, google:store.rating_google}
        } AS metadata
 """
--- llm_response/make_response.py
+++ llm_response/make_response.py
@@ -27,7 +27,7 @@ def get_llm_response(user_input):
 """
 
 """
-선진버전 : 1019
+선진버전 : 1020
 """
 
 def get_llm_response(query, response):
@@ -38,12 +38,12 @@ def get_llm_response(query, response):
     TASK:
     1. Consider the user's needs.
     2. Suggest up to three suitable restaurants from the provided context.
-    3. Ensure each recommendation truly fits the user's needs.
+    3. Ensure each recommendation truly fits the user's needs; if not don't recommend.
     4. Justify each suggestion without quoting reviews directly.
-    5. Include the restaurant's 'menu' as listed.
+    5. Include each restaurant's image using markdown and change width of 200px (ignore if NaN), rating (ignore if NaN), and menu (ignore if NaN).
     6. Mention 'Nearby tourist attractions' for each restaurant.
 
-    Question: {query}
+    QUESTION: {query}
 
     CONTEXT: {response}
 
