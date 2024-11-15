8f76f547499289b046ef524e9eba3c5f583fafd9
PassionA2Z <passiona2z@naver.com>
2024-11-15 14:51:51 +0900

    ev for streamlit

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
+import streamlit as st
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
+#load_dotenv()
 
-load_dotenv()
-
+"""
 class Config:
     neo4j_url = os.environ["NEO4J_URI"]
     neo4j_user = os.environ["NEO4J_USERNAME"]
@@ -13,5 +14,17 @@ class Config:
     store_retriever_rev_emb_k_grp = 6
     recomm_candidates_num = 6
     recomm_select_k = 2
+"""
+
+class Config:
+    neo4j_url = st.secrets["NEO4J_URI"]
+    neo4j_user = st.secrets["NEO4J_USERNAME"]
+    neo4j_password = st.secrets["NEO4J_PASSWORD"] 
+    gemini_api_key = st.secrets["KYEONGCHAN_GEMINI_API_KEY"]
+    store_retriever_rev_emb_k = 30
+    store_retriever_rev_emb_k_grp = 6
+    recomm_candidates_num = 6
+    recomm_select_k = 2
+
 
 CONFIG = Config()
